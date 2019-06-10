cce84d853279127dd86f475f067d866b2631fd45
caleb <caleb.a.mccarthy@gmail.com>
2019-06-09 22:50:23 -0500

    solved problem

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+
+
+fn main() {
+    println!("Daily Coding Problem #37 Easy. Find the powerset");
+    let mut numbers: Vec<i32> = Vec::new();
+    numbers.push(1);
+    numbers.push(2);
+    numbers.push(3);
+    
+    let output = power_set(numbers.clone());
+    println!("input:{:?}",numbers);
+    println!("powerset:{:?}",output);
+}
+
+/*
+Convert to binary,iterate to 2^n if 1 add if 0 don't add is added example
+*/
+
+fn power_set(input:Vec<i32>)-> Vec<Vec<i32>>{
+    let two:i32 = 2;
+    let length = two.pow(input.len() as u32);
+    let mut output: Vec<Vec<i32>> = Vec::new();
+
+    for x in 0..length  {
+        let mut bin:String = format!("{:b}",x);
+        let mut bin = pad_zeros(bin,(input.len() -1) as i32);
+        let mut inp:Vec<i32> = Vec::new();
+        for (i,c) in bin.chars().enumerate(){
+            
+            if c == '1' {
+                let vh = input.get(i).clone().unwrap();
+                inp.push(*vh);
+            }
+        }
+        output.push(inp);
+
+    }
+    return output;
+}
+
+fn pad_zeros(mut input:String,pad_size:i32)->String{
+    if input.len() < (pad_size +1) as usize {
+        let mut nstring = String::from("0");
+        nstring.push_str(&input.clone());
+        return pad_zeros(nstring,pad_size);
+    }
+    return input;
+}
